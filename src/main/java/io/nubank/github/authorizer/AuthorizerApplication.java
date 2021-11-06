8b72e17fc855b64945b74584433524d1bf0467ea
Ricardo Sander Lopes <ricardo.lopes@ifood.com.br>
2021-11-06 14:40:16 -0300

    Add terminal input reading and output writing

--- src/main/java/io/nubank/github/authorizer/AuthorizerApplication.java
+++ src/main/java/io/nubank/github/authorizer/AuthorizerApplication.java
@@ -1,9 +1,16 @@
 package io.nubank.github.authorizer;
 
+import java.util.Scanner;
+
 public class AuthorizerApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hell World!");
+        System.out.println("Hello World!");
+
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            System.out.println(input.nextLine());
+        }
     }
 
 }
