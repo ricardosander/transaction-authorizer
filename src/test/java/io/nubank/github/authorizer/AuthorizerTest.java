1b59b1746d9e9fa6fa0bba1ab9793e298b082ff4
Ricardo Sander Lopes <ricardo.lopes@ifood.com.br>
2021-11-09 01:19:58 -0300

    test(Authorizer): Add test to authorizer many violations

--- src/test/java/io/nubank/github/authorizer/AuthorizerTest.java
+++ src/test/java/io/nubank/github/authorizer/AuthorizerTest.java
@@ -68,7 +68,7 @@ class AuthorizerTest {
     void shouldCreateTransactionSuccessfully_whenAccountIsAlreadyCreated() {
 
         AccountCreation account = buildAccountCreation(true, 100);
-        TransactionCreation transaction = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:00.000");
+        TransactionCreation transaction = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:00");
         List<OperationRequest> requests = List.of(account, transaction);
 
         List<OperationResult> results = authorizer.execute(requests);
@@ -93,7 +93,7 @@ class AuthorizerTest {
     void shouldReturnAccountNotInitializedViolation_whenAccountIsNotCreatedBeforeTransactionCreation() {
 
         AccountCreation account = buildAccountCreation(true, 225);
-        TransactionCreation transaction = buildTransactionCreation("Uber Eats", 25, "2020-12-01T11:07:00.000");
+        TransactionCreation transaction = buildTransactionCreation("Uber Eats", 25, "2020-12-01T11:07:00");
         List<OperationRequest> requests = List.of(transaction, account, transaction);
         List<OperationResult> results = authorizer.execute(requests);
 
@@ -122,8 +122,8 @@ class AuthorizerTest {
     void shouldReturnCardNotActiveViolation_whenTryToCreateTransactionAndCardAccountIsInactive() {
 
         AccountCreation account = buildAccountCreation(false, 100);
-        TransactionCreation burgerKing = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:00.000");
-        TransactionCreation habbibs = buildTransactionCreation("Habbib's", 15, "2019-02-13T11:15:00.000");
+        TransactionCreation burgerKing = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:00");
+        TransactionCreation habbibs = buildTransactionCreation("Habbib's", 15, "2019-02-13T11:15:00");
         List<OperationRequest> requests = List.of(account, burgerKing, habbibs);
 
         List<OperationResult> results = authorizer.execute(requests);
@@ -153,9 +153,9 @@ class AuthorizerTest {
     void shouldReturnInsufficientLimitViolation_whenTransactionExceedsAccountLimit() {
 
         AccountCreation account = buildAccountCreation(true, 1000);
-        TransactionCreation vivara = buildTransactionCreation("Vivara", 1250, "2019-02-13T11:00:00.000");
-        TransactionCreation samsung = buildTransactionCreation("Samsung", 2500, "2019-02-13T11:00:01.000");
-        TransactionCreation nike = buildTransactionCreation("Nike", 800, "2019-02-13T11:01:01.000");
+        TransactionCreation vivara = buildTransactionCreation("Vivara", 1250, "2019-02-13T11:00:00");
+        TransactionCreation samsung = buildTransactionCreation("Samsung", 2500, "2019-02-13T11:00:01");
+        TransactionCreation nike = buildTransactionCreation("Nike", 800, "2019-02-13T11:01:01");
         List<OperationRequest> requests = List.of(account, vivara, samsung, nike);
 
         List<OperationResult> results = authorizer.execute(requests);
@@ -190,11 +190,11 @@ class AuthorizerTest {
     void shouldReturnHighFrequencySmallIntervalViolation_whenFourTransactionAreMadeInLessThenTowMinutes() {
 
         AccountCreation account = buildAccountCreation(true, 100);
-        TransactionCreation burgerKing = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:00.000");
-        TransactionCreation habbibs = buildTransactionCreation("Habbib's", 20, "2019-02-13T11:00:01.000");
-        TransactionCreation mcDonalds = buildTransactionCreation("McDonald's", 20, "2019-02-13T11:01:01.000");
-        TransactionCreation subway = buildTransactionCreation("Subway", 20, "2019-02-13T11:01:31.000");
-        TransactionCreation burgerKingAgain = buildTransactionCreation("Burger King", 10, "2019-02-13T12:00:00.000");
+        TransactionCreation burgerKing = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:00");
+        TransactionCreation habbibs = buildTransactionCreation("Habbib's", 20, "2019-02-13T11:00:01");
+        TransactionCreation mcDonalds = buildTransactionCreation("McDonald's", 20, "2019-02-13T11:01:01");
+        TransactionCreation subway = buildTransactionCreation("Subway", 20, "2019-02-13T11:01:31");
+        TransactionCreation burgerKingAgain = buildTransactionCreation("Burger King", 10, "2019-02-13T12:00:00");
         List<OperationRequest> requests = List.of(account, burgerKing, habbibs, mcDonalds, subway, burgerKingAgain);
 
         List<OperationResult> results = authorizer.execute(requests);
@@ -238,10 +238,10 @@ class AuthorizerTest {
     void shouldReturnDoubledTransactionViolation_whenTwoTransactionToSameMerchantAndValueAreMadeInLessThenTwoMinutes() {
 
         AccountCreation account = buildAccountCreation(true, 100);
-        TransactionCreation burgerKing = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:00.000");
-        TransactionCreation mcDonalds = buildTransactionCreation("McDonald's", 10, "2019-02-13T11:00:01.000");
-        TransactionCreation doubledBurgerKing = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:02.000");
-        TransactionCreation otherBurgerKing = buildTransactionCreation("Burger King", 15, "2019-02-13T11:00:03.000");
+        TransactionCreation burgerKing = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:00");
+        TransactionCreation mcDonalds = buildTransactionCreation("McDonald's", 10, "2019-02-13T11:00:01");
+        TransactionCreation doubledBurgerKing = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:02");
+        TransactionCreation otherBurgerKing = buildTransactionCreation("Burger King", 15, "2019-02-13T11:00:03");
         List<OperationRequest> requests = List.of(account, burgerKing, mcDonalds, doubledBurgerKing, otherBurgerKing);
 
         List<OperationResult> results = authorizer.execute(requests);
@@ -274,6 +274,80 @@ class AuthorizerTest {
         assertThat(results.get(4).getAccount().isActiveCard()).isTrue();
         assertThat(results.get(4).getAccount().getAvailableLimit()).isEqualTo(55);
         assertThat(results.get(4).getViolations()).isEmpty();
+    }
+
+    @Test
+    void shouldReturnMultipleViolations_whenMultipleViolationsAreFound() {
+
+        AccountCreation account = buildAccountCreation(true, 100);
+        TransactionCreation transaction = buildTransactionCreation("McDonald's", 10, "2019-02-13T11:00:01");
+        TransactionCreation transaction2 = buildTransactionCreation("Burger King", 20, "2019-02-13T11:00:02");
+        TransactionCreation transaction3 = buildTransactionCreation("Burger King", 5, "2019-02-13T11:00:07");
+        TransactionCreation transaction4 = buildTransactionCreation("Burger King", 5, "2019-02-13T11:00:08");
+        TransactionCreation transaction5 = buildTransactionCreation("Burger King", 150, "2019-02-13T11:00:18");
+        TransactionCreation transaction6 = buildTransactionCreation("Burger King", 190, "2019-02-13T11:00:22");
+        TransactionCreation transaction7 = buildTransactionCreation("Burger King", 15, "2019-02-13T12:00:27");
+        List<OperationRequest> requests = List.of(
+               account,
+               transaction,
+               transaction2,
+               transaction3,
+               transaction4,
+               transaction5,
+               transaction6,
+               transaction7
+        );
+
+        List<OperationResult> results = authorizer.execute(requests);
+
+        assertThat(results).isNotEmpty();
+        assertThat(results.size()).isEqualTo(8);
+
+        assertThat(results.get(0).getAccount()).isNotNull();
+        assertThat(results.get(0).getAccount().isActiveCard()).isTrue();
+        assertThat(results.get(0).getAccount().getAvailableLimit()).isEqualTo(100);
+        assertThat(results.get(0).getViolations()).isEmpty();
+
+        assertThat(results.get(1).getAccount()).isNotNull();
+        assertThat(results.get(1).getAccount().isActiveCard()).isTrue();
+        assertThat(results.get(1).getAccount().getAvailableLimit()).isEqualTo(90);
+        assertThat(results.get(1).getViolations()).isEmpty();
+
+        assertThat(results.get(2).getAccount()).isNotNull();
+        assertThat(results.get(2).getAccount().isActiveCard()).isTrue();
+        assertThat(results.get(2).getAccount().getAvailableLimit()).isEqualTo(70);
+        assertThat(results.get(2).getViolations()).isEmpty();
+
+        assertThat(results.get(3).getAccount()).isNotNull();
+        assertThat(results.get(3).getAccount().isActiveCard()).isTrue();
+        assertThat(results.get(3).getAccount().getAvailableLimit()).isEqualTo(65);
+        assertThat(results.get(3).getViolations()).isEmpty();
+
+        assertThat(results.get(4).getAccount()).isNotNull();
+        assertThat(results.get(4).getAccount().isActiveCard()).isTrue();
+        assertThat(results.get(4).getAccount().getAvailableLimit()).isEqualTo(65);
+        assertThat(results.get(4).getViolations()).isNotEmpty();
+        assertThat(results.get(4).getViolations()).contains("high-frequency-small-interval");
+        assertThat(results.get(4).getViolations()).contains("doubled-transaction");
+
+        assertThat(results.get(5).getAccount()).isNotNull();
+        assertThat(results.get(5).getAccount().isActiveCard()).isTrue();
+        assertThat(results.get(5).getAccount().getAvailableLimit()).isEqualTo(65);
+        assertThat(results.get(5).getViolations()).isNotEmpty();
+        assertThat(results.get(5).getViolations()).contains("insufficient-limit");
+        assertThat(results.get(5).getViolations()).contains("high-frequency-small-interval");
+
+        assertThat(results.get(6).getAccount()).isNotNull();
+        assertThat(results.get(6).getAccount().isActiveCard()).isTrue();
+        assertThat(results.get(6).getAccount().getAvailableLimit()).isEqualTo(65);
+        assertThat(results.get(6).getViolations()).isNotEmpty();
+        assertThat(results.get(6).getViolations()).contains("insufficient-limit");
+        assertThat(results.get(6).getViolations()).contains("high-frequency-small-interval");
+
+        assertThat(results.get(7).getAccount()).isNotNull();
+        assertThat(results.get(7).getAccount().isActiveCard()).isTrue();
+        assertThat(results.get(7).getAccount().getAvailableLimit()).isEqualTo(50);
+        assertThat(results.get(7).getViolations()).isEmpty();
     }
 
     private AccountCreation buildAccountCreation(boolean isCardActive, int availableLimit) {
